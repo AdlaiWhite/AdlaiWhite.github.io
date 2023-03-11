078f134cf5dd932fb2e9eff4312fd05997fd2346
Adlai <adwh5430@colorado.edu>
2023-03-11 14:16:36 -0700

    ok

---
+++ js/website.js
@@ -0,0 +1,8 @@
+const hamburgerButtom = document.getElementById('hamburger')
+const navList =document.getElementById('nav-list')
+
+function toggleButton(){
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click', toggleButton)
